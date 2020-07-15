fe1402eca7989fe8b9acb7973be64b900b7c4641
Slamma009 <alsnurb@gmail.com>
2020-07-15 11:01:27 -0500

    Update AngularJS routing

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
     .when("/", {
         controller: "surveyController",
